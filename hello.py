fcdf728e88f0e94f0ddc540acd295579c37d86ee
Uran Onuk <uran.onuk@mail.utoronto.ca>
2020-09-25 20:19:18 -0400

    Fixed issue with name "Uran"

--- hello.py
+++ hello.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return '<h1>Hello World!</h1>'
-  
+
 @app.route('/user/<name>')
 def user(name):
-  return '<h1>Hello, %s!</h1>' % name
+  return '<h1>Hello, %s!</h1>' % 'Uran'
   
 if __name__ == '__main__':
   app.run(debug=True)
